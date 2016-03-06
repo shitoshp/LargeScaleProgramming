c72d8da4c8bfcd3c74d9a825b23b40a87c21dbb1
shitoshp <shitosh@yahoo.com>
2016-03-05 19:42:49 -0500

    addentry working

--- Phone Directory/src/main/java/PhoneDirectory.java
+++ Phone Directory/src/main/java/PhoneDirectory.java
@@ -1,20 +1,30 @@
+import java.io.*;
+import java.util.*;
 
 public class PhoneDirectory {
 	
-	public void addEntry(String name, String number){
+	public void addEntry(String name, String number) throws IOException{
 		
+		Properties my_prop = new Properties();
+		OutputStream output = new FileOutputStream("src/main/resources/phone.properties", true);
+		
+		my_prop.setProperty(name, number);
+		
+		my_prop.store(output, null);  //store value
+		output.close();
 	}
 	
-	public void deleteEntry(String name){
-		
-	}
-	
-	public String getNumber(String name){
-		
-	}
-	
-	public void changeEntry(String name, String number){
-		
-	}
-	
+//	public void deleteEntry(String name){
+//		
+//	}
+//	
+//	public String getNumber(String name){
+//		
+//	}
+//	
+//	public void changeEntry(String name, String number){
+//		
+//	}
+//	
 }
+
